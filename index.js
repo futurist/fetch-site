3047f6356759591488d83aede727982bebe80309
futurist <26634873@qq.com>
2018-05-09 13:28:27 +0800

    1.2.4 default networkidle2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const makeDir = require('make-dir')
 const isBase64 = require('is-base64')
 const mime = require('mime-types')
 const writeFile = promisify(fs.writeFile)
+const {assign} = Object
 
 async function main({
   dir,
@@ -20,11 +21,19 @@ async function main({
   onAfterOpen,
   onFinish,
 }={}) {
+  // check options
   if(!dir||!url) throw 'dir and url cannot be empty'
+  // puppeteer cannot run inside Docker, since the sandbox
+  // launchOption = assign({args: ['--no-sandbox']}, launchOption)
+  openOption = assign({waitUntil: 'networkidle2'}, openOption)
+
+  // launch puppeteer
   const browser = await puppeteer.launch(launchOption)
   const responseData = []
   await makeDir(dir)
   const page = await browser.newPage()
+
+  // response hook
   page.on('response', async response => {
     const request = response.request()
     const url = response.url()
@@ -68,12 +77,16 @@ async function main({
     await makeDir(dirname(filePath))
     await writeFile(filePath, body, {encoding: toNodeEncoding(charset)})
   })
+
+  // goto url
   onBeforeOpen && await onBeforeOpen(page)
   await page.goto(url, openOption)
   onAfterOpen && await onAfterOpen(page)
   shot && await page.screenshot({
     path: joinPath(dir, shot)
   })
+
+  // finish work
   await browser.close()
   await writeFile(joinPath(dir,'response.json'), JSON.stringify(responseData), 'utf8')
   onFinish && await onFinish(page)
